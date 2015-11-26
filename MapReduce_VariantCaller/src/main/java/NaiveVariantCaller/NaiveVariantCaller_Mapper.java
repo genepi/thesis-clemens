8de8c50a6b8807ce54aae9df50e34ba1b4681a1b
Clemens Banas <clemens.banas@holzweg2.com>
2015-11-26 19:03:39 +0100

    variantcaller: map reduce (java) code

---
+++ MapReduce_VariantCaller/src/main/java/NaiveVariantCaller/NaiveVariantCaller_Mapper.java
@@ -0,0 +1,100 @@
+package NaiveVariantCaller;
+
+import htsjdk.samtools.SAMRecord;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.seqdoop.hadoop_bam.FileVirtualSplit;
+import org.seqdoop.hadoop_bam.SAMRecordWritable;
+import utils.NaiveVariantCallerKeyWritable;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * master-thesis Clemens Banas
+ * Organization: DBIS - University of Innsbruck
+ * Created 02.10.15.
+ */
+public class NaiveVariantCaller_Mapper extends org.apache.hadoop.mapreduce.Mapper<LongWritable, SAMRecordWritable, NaiveVariantCallerKeyWritable, Text> {
+    private static final int MIN_BASE_QUAL = 30;
+    private static final int MIN_MAP_QUAL = 30;
+    private static final int MIN_ALIGN_QUAL = 30;
+
+    private static final int MIN_READ_LENGTH = 25;
+
+    private static final char BASE_A = 'A';
+    private static final char BASE_C = 'C';
+    private static final char BASE_G = 'G';
+    private static final char BASE_T = 'T';
+
+    @Override
+    protected void map(LongWritable key, SAMRecordWritable value, Context context) throws IOException, InterruptedException {
+        final String sampleIdentifier = ((FileVirtualSplit)context.getInputSplit()).getPath().getName();
+
+        SAMRecord samRecord = value.get();
+        final byte[] readBases = samRecord.getReadBases();
+        String sequence = new String(readBases, StandardCharsets.UTF_8);
+
+        if (readFullfillsRequirements(samRecord)) {
+            for (int i = 0; i < sequence.length(); i++) {
+                if (baseQualitySufficient((samRecord.getBaseQualities()[i]))) {
+                NaiveVariantCallerKeyWritable outputKey = new NaiveVariantCallerKeyWritable(sampleIdentifier, samRecord.getReferencePositionAtReadPosition(i+1));
+                    switch (sequence.charAt(i)) {
+                        case BASE_A:
+                            context.write(outputKey, new Text(String.valueOf(BASE_A)));
+                            break;
+                        case BASE_C:
+                            context.write(outputKey, new Text(String.valueOf(BASE_C)));
+                            break;
+                        case BASE_G:
+                            context.write(outputKey, new Text(String.valueOf(BASE_G)));
+                            break;
+                        case BASE_T:
+                            context.write(outputKey, new Text(String.valueOf(BASE_T)));
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+    }
+
+    private boolean readFullfillsRequirements(SAMRecord samRecord) {
+        return mappingQualitySufficient(samRecord.getMappingQuality())
+                && alignmentQualitySufficient(samRecord)
+                && !samRecord.getReadUnmappedFlag()
+                && !samRecord.getDuplicateReadFlag()
+                && samRecord.getReadLength() > MIN_READ_LENGTH;
+    }
+
+    private boolean mappingQualitySufficient(int mapQual) {
+        // 255 means that mapping quality is not available
+        if(mapQual >= MIN_MAP_QUAL || mapQual == 255) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean alignmentQualitySufficient(SAMRecord samRecord) {
+        try {
+            int alignmentQuality = samRecord.getIntegerAttribute("AS");
+            if(alignmentQuality >= MIN_ALIGN_QUAL) {
+                return true;
+            }
+        } catch (NullPointerException e) {
+            //attribute value is not mandatory
+            //therefore return true
+            return true;
+        }
+        return false;
+    }
+
+    private boolean baseQualitySufficient(int baseQual) {
+        if(baseQual >= MIN_BASE_QUAL) {
+            return true;
+        }
+        return false;
+    }
+
+}
---
+++ MapReduce_VariantCaller/src/main/java/utils/NaiveVariantCallerKeyWritable.java
@@ -0,0 +1,101 @@
+package utils;
+
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableComparator;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+/**
+ * master-thesis Clemens Banas
+ * Organization: DBIS - University of Innsbruck
+ * Created 23.10.15.
+ */
+public class NaiveVariantCallerKeyWritable implements WritableComparable<NaiveVariantCallerKeyWritable> {
+    private String sampleIdentifier;
+    private int position;
+
+    public NaiveVariantCallerKeyWritable() {
+
+    }
+
+    public NaiveVariantCallerKeyWritable(String sampleIdentifier, int position) {
+        this.sampleIdentifier = sampleIdentifier;
+        this.position = position;
+    }
+
+    public void write(DataOutput out) throws IOException {
+        out.writeInt(position);
+        out.writeUTF(sampleIdentifier);
+    }
+
+    public void readFields(DataInput in) throws IOException {
+        position = in.readInt();
+        sampleIdentifier = in.readUTF();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        NaiveVariantCallerKeyWritable that = (NaiveVariantCallerKeyWritable) o;
+
+        if (position != that.position) return false;
+        return !(sampleIdentifier != null ? !sampleIdentifier.equals(that.sampleIdentifier) : that.sampleIdentifier != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = sampleIdentifier != null ? sampleIdentifier.hashCode() : 0;
+        result = 31 * result + position;
+        return result;
+    }
+
+    public String getSampleIdentifier() {
+        return sampleIdentifier;
+    }
+
+    public void setSampleIdentifier(String sampleIdentifier) {
+        this.sampleIdentifier = sampleIdentifier;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    @Override
+    public String toString() {
+        return this.sampleIdentifier + "," + this.position;
+    }
+
+    /** Compares two NaiveVariantCallerKeyWritables. */
+    public int compareTo(NaiveVariantCallerKeyWritable that) {
+        if(this.sampleIdentifier.compareTo(that.sampleIdentifier) != 0) {
+            return this.sampleIdentifier.compareTo(that.sampleIdentifier);
+        } else {
+            return (this.position<that.position ? -1 : (this.position==that.position ? 0 : 1));
+        }
+    }
+
+    public static class NaiveVariantCallerKeyWritableComparator extends WritableComparator {
+        public NaiveVariantCallerKeyWritableComparator() {
+            super(NaiveVariantCallerKeyWritable.class);
+        }
+
+        public int compare(byte[] b1, int s1, int l1, byte[] b2, int s2, int l2) {
+            return compareBytes(b1, s1, l1, b2, s2, l2);
+        }
+    }
+
+    static { // register this comparator
+        WritableComparator.define(NaiveVariantCallerKeyWritable.class,
+                new NaiveVariantCallerKeyWritableComparator());
+    }
+}
