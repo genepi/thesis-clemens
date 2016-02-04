2e5cc805a019bc7e6749f2da405e114b9eccc61d
Clemens Banas <clemens.banas@holzweg2.com>
2016-02-04 14:03:15 +0100

    added MR combiner per BaseQuality job

---
+++ MapReduce_FastQ_BaseQual_Combiner/src/main/java/baseQual/FastQ_PerBaseQual_Mapper.java
@@ -0,0 +1,30 @@
+package baseQual;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.seqdoop.hadoop_bam.SequencedFragment;
+import utils.QualityCountHelperWritable;
+
+import java.io.IOException;
+
+/**
+ * master-thesis Clemens Banas
+ * Organization: DBIS - University of Innsbruck
+ * Created 04.02.16.
+ */
+public class FastQ_PerBaseQual_Mapper extends org.apache.hadoop.mapreduce.Mapper<Object, SequencedFragment, IntWritable, QualityCountHelperWritable> {
+    private static final int OFFSET = 33;
+
+    @Override
+    protected void map(Object key, SequencedFragment value, Context context) throws IOException, InterruptedException {
+        final Text quality = value.getQuality();
+        for (int i = 0; i < quality.getLength(); i++) {
+            context.write(new IntWritable(i + 1), new QualityCountHelperWritable(getCorrespondingIntValue(quality.charAt(i))));
+        }
+    }
+
+    private int getCorrespondingIntValue(int qualVal) {
+        return qualVal - FastQ_PerBaseQual_Mapper.OFFSET;
+    }
+
+}
---
+++ MapReduce_FastQ_BaseQual_Combiner/src/main/java/utils/QualityCountHelperWritable.java
@@ -0,0 +1,48 @@
+package utils;
+
+import org.apache.hadoop.io.Writable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+/**
+ * master-thesis Clemens Banas
+ * Organization: DBIS - University of Innsbruck
+ * Created 04.02.16.
+ */
+public class QualityCountHelperWritable implements Writable {
+    private int sumOfQualityValues = 0;
+    private int numberOfQualityValues = 0;
+
+    public QualityCountHelperWritable() {
+    }
+
+    public QualityCountHelperWritable(int sumOfQualityValues, int numberOfQualityValues) {
+        this.sumOfQualityValues = sumOfQualityValues;
+        this.numberOfQualityValues = numberOfQualityValues;
+    }
+
+    public QualityCountHelperWritable(int sumOfQualityValues) {
+        this(sumOfQualityValues, 0);
+    }
+
+    public int getSumOfQualityValues() {
+        return sumOfQualityValues;
+    }
+
+    public int getNumberOfQualityValues() {
+        return numberOfQualityValues;
+    }
+
+    public void write(DataOutput out) throws IOException {
+        out.writeInt(sumOfQualityValues);
+        out.writeInt(numberOfQualityValues);
+    }
+
+    public void readFields(DataInput in) throws IOException {
+        this.sumOfQualityValues = in.readInt();
+        this.numberOfQualityValues = in.readInt();
+    }
+
+}
